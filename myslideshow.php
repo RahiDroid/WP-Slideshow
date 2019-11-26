6dbc6f7f85875c773dd4e73a953fe1255d8bb981
Tan-007 <rahi.prajapati1811@gmail.com>
2019-11-26 17:11:06 +0530

    added plugin description

--- myslideshow.php
+++ myslideshow.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       MySlideShow
  * Plugin URI:        rahicodes.wordpress.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.1
+ * Description:       This plugin allows you to display a fully functional and responsive slideshow anywhere in your site by just using a shortcode!
+ * Version:           1.0.3
  * Author:            Rahi Prajapati
  * Author URI:        rahicodes.wordpress.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MYSLIDESHOW_VERSION', '1.0.1' );
+define( 'MYSLIDESHOW_VERSION', '1.0.3' );
 
 /**
  * Global options
